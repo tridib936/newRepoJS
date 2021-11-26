a53c0943283f0cc3a963acc2aae783db1bdeb302
Tridib Mazumder <61946877+tridib936@users.noreply.github.com>
2021-11-26 18:17:14 +0530

    Delete circle.js

--- circle.js
+++
@@ -1,9 +0,0 @@
-class Circle extends Shape {
-    constructor(x, y, r) {
-        super(x, y);
-        this.r = r;
-    }
-    calculateArea() {
-        return Math.PI * this.r * this.r;
-    }
-}
